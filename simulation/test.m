e5370c204587b0e52f7d86e6f6ab1b3782dda9d0
Sagar G V <sagar.writeme@gmail.com>
2014-03-31 13:12:35 +0530

    Testing full signal chain without body model

--- simulation/test.m
+++ simulation/test.m
@@ -1,6 +1,8 @@
-t = 0:0.001:0.8;
+t = 0:0.001:0.5;
 %plot(adc(bodymodel(t, 1.5)));
 %plot(adc(1.5 + 1*sin(2*pi*10*t)));
 %plot(attenuator(1.6 + 1*sin(2*pi*10*t)));
 %plot(dcamp(1.65 + 0.001*sin(2*pi*10*t)));
-plot(adc(dcamp(1.65 + 0.005*sin(2*pi*10*t))));
+%plot(adc(dcamp(1.65 + 0.005*sin(2*pi*10*t))));
+
+plot(adc(dcamp(attenuator(dac(512 + 10*sin(2*pi*6*t))))));
