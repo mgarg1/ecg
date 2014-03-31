229cc3fe8444899a106edc8dee509ced2b2c602c
Sagar G V <sagar.writeme@gmail.com>
2014-03-31 12:31:49 +0530

    DC Amplifier model

--- simulation/test.m
+++ simulation/test.m
@@ -1,3 +1,6 @@
 t = 0:0.001:0.8;
 %plot(adc(bodymodel(t, 1.5)));
-plot(adc(1.5 + 0.5*sin(2*pi*10*t)));
+%plot(adc(1.5 + 1*sin(2*pi*10*t)));
+%plot(attenuator(1.6 + 1*sin(2*pi*10*t)));
+%plot(dcamp(1.65 + 0.001*sin(2*pi*10*t)));
+plot(adc(dcamp(1.65 + 0.005*sin(2*pi*10*t))));
