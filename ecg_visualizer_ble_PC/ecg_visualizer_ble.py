c0c943a4e89bbfe6925eb0b4e804559b05728dd9
Sagar G V <sagar.writeme@gmail.com>
2014-06-03 18:31:15 +0530

    Log messages

--- ecg_visualizer_ble_PC/ecg_visualizer_ble.py
+++ ecg_visualizer_ble_PC/ecg_visualizer_ble.py
@@ -1,11 +1,19 @@
 import CC2540ble as ble
 
 def main():
+    print "Connecting to BLE Dongle . . ."
     bt = ble.BTDongle(port='/dev/ttyACM0')
+
+    print "Discovering BLE devices in the vicinity . . ."
     devs = bt.discover()
-    print devs
+    print "BLE Devices found: ", devs
+
+    print "Establishing link to the first device found . . ."
     print bt.link(devs[0])
+
+    print "Enabling notifications . . ."
     print bt.enableNotifications('0x002F')
+
     for evt in bt.pollNotifications():
         print evt
 
