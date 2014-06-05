0a3655f53aa920bb962d41d9d10095658ddeef84
Sagar G V <sagar.writeme@gmail.com>
2014-06-05 12:18:18 +0530

    converting raw vals to 16-bit ADC readings

--- ecg_visualizer_ble_PC/ecg_visualizer_ble_nograph.py
+++ ecg_visualizer_ble_PC/ecg_visualizer_ble_nograph.py
@@ -18,7 +18,10 @@ def main():
     print bt.enableNotifications('0x002F')
 
     for evt in bt.pollNotifications():
-        print evt
+        f2s = lambda x: x if x < 2**13 or x >= 65530 else (-2**14 + x)
+        vals = [ f2s(lsb + 256*msb) for (lsb, msb) in zip(evt[::2], evt[1::2]) ]
+        vals = [ val for val in vals if val < 65530 ]
+        print vals
 
 if __name__ == '__main__':
     main()
